d2d171ad09cf2d256e2c9a314059e3c1cab8d11b
nateh64 <7nhuzenis@stpatrick.org>
2016-10-12 13:57:20 -0500

    Add files via upload

---
+++ AppObject.js
@@ -0,0 +1,40 @@
+function App(n,m,s){
+
+    //Declare your instance variables here.
+    this.name =n;
+    this.memory =m;
+    this.state =s;
+
+    //Declare the instance function open here.
+    this.open = function(){
+      this.state = "active"
+
+
+    };
+    //Declare the instance function sleep here.
+    this.sleep(){
+      if(this.state = "active"){
+        this.state = "sleep";
+        this.memory
+      }
+    }
+
+
+
+    //Declare the instance function active here.
+
+
+
+    //Declare the instance function close here.
+
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
