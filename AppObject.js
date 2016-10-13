212783b199efc9671cec31b0af8892ed34889088
nateh64 <7nhuzenis@stpatrick.org>
2016-10-13 13:04:48 -0500

    Add files via upload
    Finished 4 functions and objects

--- AppObject.js
+++ AppObject.js
@@ -15,25 +15,40 @@ function App(n,m,s){
     this.sleep(){
       if(this.state = "active"){
         this.state = "sleep";
-        this.memory
+        this.memory = this.memory / 2;
+
       }
     }
 
 
 
     //Declare the instance function active here.
+    this.active == function(){
+      if(this.state = "active"){
+        this.state = "active";
+        this.memory = this.memory *2;
 
+      }
+
+    }
 
 
     //Declare the instance function close here.
-
+    this.close == function(){
+      if (this.state != null){
+        this.state = null;
+        this.memory = 0;
+      }
+    }
 
 
 
 }
 
 //Use the constructor to create 4 App objects below.
-
+var firstapp = new App("Gears of War",400000,"active");
+var secondapp = new App("NBA2k17", 500000, "active");
+var thirdapp = new App("MW3", 160000, "active");
 
 
 //Declare an array named appList, and place all 4 of your apps in that array.
